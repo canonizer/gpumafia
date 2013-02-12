0e920c379035293d5e1c9bce08be8f58af869e9c
Andrew V. Adinetz <adinetz@gmail.com>
2013-02-12 11:26:30 +0100

    modified expreiment-08 to run without job management system

--- exp/experiment-08/experiment-08.sh
+++ exp/experiment-08/experiment-08.sh
@@ -13,24 +13,15 @@ make -C ../../utils/clugen
 
 # clean cluster data and generate new data
 DATA_DIR=~/try/mafia/cluster-10m-10d
+DATA_FILE=$DATA_DIR/cluster-10.dat
 
 mkdir -p $DATA_DIR
-../../utils/clugen/bin/clugen -n10000000 -d10 -k10 $DATA_DIR/cluster-10.dat
+../../utils/clugen/bin/clugen -n10000000 -d10 -k10 $DATA_FILE
 
-#export GOMP_CPU_AFFINITY="0-23"
+#export GOMP_CPU_AFFINITY="0-32"
 
 # generate profile data for different number of OpenMP threads
-for nt in {1..24}; do
-		cp cppmafia-omp.sh cppmafia-tmp-0.sh
-		echo export OMP_NUM_THREADS=$nt >> cppmafia-tmp-0.sh
-		echo mpiexec -np '$NSLOTS' ../../cppmafia/bin/cppmafia --timing \
-				$DATA_DIR/cluster-10.dat >> cppmafia-tmp-0.sh
-		# submit and wait for completion
-		TASK_ID=`msub -qdevel cppmafia-tmp-0.sh`
-		while qstat $TASK_ID 2>&1>/dev/null; do 
-				sleep 1
-		done
-		# get the time data
-		mv cppmafia-omp-0.out time-$nt.log
-		rm cppmafia-omp-0.err
+for nt in {1..32}; do
+		OMP_NUM_THREADS=$nt ../../cppmafia/bin/cppmafia --timing $DATA_FILE \
+				>> time-$nt.log
 done
